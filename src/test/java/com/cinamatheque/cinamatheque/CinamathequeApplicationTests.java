bf0612cb8513fb47675fd136385892e7a673ad8b
delm0128 <e2palmes@gmail.com>
2022-12-30 00:29:39 +0100

    init the project

---
+++ src/test/java/com/cinamatheque/cinamatheque/CinamathequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinamatheque.cinamatheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinamathequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
