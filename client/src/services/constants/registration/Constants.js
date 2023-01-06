c910cd49fdd498c0b61f686f4a1872af509b9d3d
gloatingorc <gloatingorc@gmail.com>
2023-01-06 16:42:11 +0100

    validation du formulaire register

---
+++ client/src/services/constants/registration/Constants.js
@@ -0,0 +1,47 @@
+
+export const initialValues = {
+    first_name:"",
+    last_name:"",
+    username:"",
+    email:"",
+    password:"",
+    confirm_password:"",
+    agree_terms:"",
+}
+
+export const regex_email = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+export const regex_password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])(?=.*[a-zA-Z]).{8,}$/;
+export const regex_username = /^(?=.*[a-zA-Z]).{6,}$/;
+
+export const validate = (userValues, agreeTerms)=>{
+    const errors = {};
+
+    if(!userValues.first_name){
+        errors.first_name = "Vous devez rentrer votre prénom"
+    }else{delete errors.first_name}
+
+    if(!userValues.last_name){
+        errors.last_name = "Vous devez rentrer votre nom"
+    }else{delete errors.last_name}
+
+    if(!regex_username.test(userValues.username)){
+        errors.username = "L'identifiant doit avoir au moins 6 charactéres"
+    }else{delete errors.username}
+
+    if(!regex_email.test(userValues.email)){
+        errors.email = "Entrer un e-mail valide"
+    }else{delete errors.email}
+
+    if(!regex_password.test(userValues.password)){
+        errors.password = "Votre mot de passe doit contenir au moins un charactére majuscule, un charactére minuscule, un chiffre et un un charactére spécial"
+    }
+    else{delete errors.password}
+
+    if(userValues.password !== userValues.confirm_password){
+        errors.confirm_password = "Les deux mots de passe ne se ressemble pas"
+    }else{delete errors.confirm_password}
+    if(!agreeTerms){
+        errors.agree_terms = "Vous devez accépter les conditions d'utilisation de l'application"
+    }else{delete errors.agree_terms}
+    return errors;
+}
--- client/src/views/registration/Register.js
+++ client/src/views/registration/Register.js
@@ -1,9 +1,42 @@
 import {TextFieldMedium} from "../../components/forms/TextField/TextFieldMedium";
 import {TextFieldLarge} from "../../components/forms/TextField/TextFieldLarge";
 import {FormSubmit} from "../../components/buttons/FormSubmit";
-import {CheckBoxField} from "../../components/CheckBox/CheckBoxField";
+import {CheckBoxField} from "../../components/forms/CheckBox/CheckBoxField";
+import {initialValues, validate} from "../../services/constants/registration/Constants"
+import {useState, useEffect} from "react";
 
 function Register(){
+
+    const [userValues, setUserValues] = useState(initialValues);
+    const [agreeTerms, setAgreeTerms] = useState(false);
+    const [formError, setFormError] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleChange = (e)=>{
+        const {name, value} = e.target
+        setUserValues({...userValues,[name]:value});
+    }
+
+    const handleChecked = (e)=>{
+        setAgreeTerms(e.target.checked);
+    }
+
+    const submitHandler =(e)=>{
+        e.preventDefault();
+        if(Object.keys(formError).length === 0){
+
+            console.log("form submitted");
+        }else{
+            setIsSubmit(true);
+            console.log("form have errors");
+        }
+    }
+
+    useEffect(()=>{
+        setFormError(validate(userValues, agreeTerms));
+    },[userValues,agreeTerms])
+
+
     return (
         <div className="mx-5 py-12 my-0 flex flex-col items-center">
             <section className="py-6 px-20">
@@ -12,20 +45,84 @@ function Register(){
                     Inscription
                 </h1>
 
+                {isSubmit &&
+
+                    <div
+                        className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg font-bold"
+                        role="alert">
+                        <span className="font-medium">Le formulaire que vous venez de soumettre contient des erreurs</span>
+                    </div>
+
+                }
                 {/* registration form*/}
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className="flex flex-col max-w-xl">
 
                         {/* first name and last name section*/}
                         <div className="flex justify-between flex-wrap">
-                            <TextFieldMedium label={"Prénom"} placeholder={"Entrer votre prénom"}/>
-                            <TextFieldMedium label={"Nom"} placeholder={"Entrer votre nom"}/>
+                            <TextFieldMedium label={"Prénom"}
+                                             type={"text"}
+                                             placeholder={"Entrer votre prénom"}
+                                             name={"first_name"}
+                                             values={userValues.first_name}
+                                             formError={formError.first_name}
+                                             handleChange={handleChange}
+
+                            />
+                            <TextFieldMedium label={"Nom"}
+                                             type={"text"}
+                                             placeholder={"Entrer votre nom"}
+                                             name={"last_name"}
+                                             values={userValues.last_name}
+                                             formError={formError.last_name}
+                                             handleChange={handleChange}
+
+                            />
+
                         </div>
-                        <TextFieldLarge label={"Identifiant"} placeholder={"Entrer votre identifiant"}/>
-                        <TextFieldLarge label={"Email"} placeholder={"Entrer votre email"}/>
-                        <TextFieldLarge label={"Mot de passe"} placeholder={"Entrer votre mot de passe"}/>
-                        <TextFieldLarge label={"Confirmer votre mot de passe"} placeholder={"Confirmer votre mot de passe"}/>
-                        <CheckBoxField/>
+                        <TextFieldLarge label={"Identifiant"}
+                                        type={"text"}
+                                        placeholder={"Entrer votre identifiant"}
+                                        name={"username"}
+                                        values={userValues.username}
+                                        formError={formError.username}
+                                        handleChange={handleChange}
+
+
+                        />
+                        <TextFieldLarge label={"Email"}
+                                        type={"text"}
+                                        placeholder={"Entrer votre email"}
+                                        name={"email"}
+                                        values={userValues.email}
+                                        formError={formError.email}
+                                        handleChange={handleChange}
+
+                        />
+                        <TextFieldLarge label={"Mot de passe"}
+                                        type={"password"}
+                                        placeholder={"Entrer votre mot de passe"}
+                                        name={"password"}
+                                        formError={formError.password}
+                                        values={userValues.password}
+                                        handleChange={handleChange}
+
+                        />
+                        <TextFieldLarge label={"Confirmer votre mot de passe"}
+                                        type={"password"}
+                                        placeholder={"Confirmer votre mot de passe"}
+                                        name={"confirm_password"}
+                                        values={userValues.confirm_password}
+                                        formError={formError.confirm_password}
+                                        handleChange={handleChange}
+
+                        />
+                        <CheckBoxField
+                                        checked={agreeTerms}
+                                        formError={formError.agree_terms}
+                                        handleChecked={handleChecked}
+
+                        />
                         <FormSubmit />
                         <p className="mt-7">
                             Vous avez déja un compte ? <a href="#login" className="text-red-600 hover:underline">Connectez vous</a>
