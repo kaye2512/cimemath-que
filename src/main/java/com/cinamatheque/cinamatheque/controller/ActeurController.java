ec626539c9fa79693838d287d36734bade604e95
kaye romuald <kayeromy@gmail.com>
2023-01-30 01:16:54 +0100

    acteur

--- src/main/java/com/cinamatheque/cinamatheque/controller/ActeurController.java
+++ src/main/java/com/cinamatheque/cinamatheque/controller/ActeurController.java
@@ -1,13 +1,26 @@
 package com.cinamatheque.cinamatheque.controller;
 
 import com.cinamatheque.cinamatheque.model.Acteur;
+import com.cinamatheque.cinamatheque.service.ActeurService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/acteur")
 @AllArgsConstructor
 public class ActeurController {
+    @Autowired
+    private ActeurService service;
+    @PostMapping
+    public Acteur CreateActeur (@RequestBody Acteur acteur){
+        return service.addActeur(acteur);
+    }
+    @GetMapping
+    public List<Acteur> getActeur() {
+        return service.findAllActeur();
+    }
 
 }
