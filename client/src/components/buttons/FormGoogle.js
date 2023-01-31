140f8df32122889055e4905e478ec4fc3f0508a8
gloatingorc <gloatingorc@gmail.com>
2023-01-31 20:06:16 +0100

    add email field in the google auth response

--- client/src/components/buttons/FormGoogle.js
+++ client/src/components/buttons/FormGoogle.js
@@ -8,29 +8,23 @@ export const FormGoogle = ({type}) => {
     const navigate = useNavigate();
     return (
         <LoginSocialGoogle client_id="955538355623-dok0i9d3ndq8ruds7k3av7qgjurc5clj.apps.googleusercontent.com"
+                           scope="email"
                            onReject={(err)=>console.log(err)}
                            onResolve={(res)=>{
-                               console.log(res.data);
-                               let username
                                const {
                                    family_name,
                                    given_name,
                                    email,
-                                   sub
                                } = res.data;
-                               if(!email){
-                                   username = sub;
-                               }else{
-                                   username = email.split("@",1)[0];
-                               }
 
                                const userValues = {
                                    firstname:given_name,
                                    lastname:family_name,
-                                   email:"",
-                                   username:username,
+                                   email:email,
+                                   username:email.split("@",1)[0],
                                    provider:"google"
                                }
+                               console.log(userValues);
                                 ApiController(type,userValues).then((res)=>{
                                     if(res){
                                         navigate("/home");
