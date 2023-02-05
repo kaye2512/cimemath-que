f22a02baf9ea3905b92bb85fbbf957d1721bef24
kaye romuald <kayeromy@gmail.com>
2023-02-05 14:51:34 +0100

    remove npm inside login.js

--- client/src/views/connexion/Login.js
+++ client/src/views/connexion/Login.js
@@ -1,4 +1,4 @@
-npm import{TextFieldLarge} from "../../components/forms/TextField/TextFieldLarge";
+import{TextFieldLarge} from "../../components/forms/TextField/TextFieldLarge";
 import {FormGoogle} from "../../components/buttons/FormGoogle";
 import {FormTwitter} from "../../components/buttons/FormTwitter";
 import {FormFacebook} from "../../components/buttons/FormFacebook";
