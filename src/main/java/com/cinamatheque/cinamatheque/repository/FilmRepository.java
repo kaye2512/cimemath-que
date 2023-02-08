cbd008552dc170296f38acead7925681051476a6
delm0128 <e2palmes@gmail.com>
2023-02-08 11:42:11 +0100

    Get films by pagination

--- src/main/java/com/cinamatheque/cinamatheque/repository/FilmRepository.java
+++ src/main/java/com/cinamatheque/cinamatheque/repository/FilmRepository.java
@@ -1,23 +1,14 @@
 package com.cinamatheque.cinamatheque.repository;
 
-import com.cinamatheque.cinamatheque.model.Acteur;
 import com.cinamatheque.cinamatheque.model.Film;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public interface FilmRepository
         extends MongoRepository<Film, String>{
-    Map<String, Acteur> roles = new HashMap<>();
     Film findByPubDate(Date pubDate);
-
     List<Film> findByTitle(String title);
-
-
-
-
 }
