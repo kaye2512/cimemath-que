e0d392ffdf4795db65bf11eb582780fcc20328b3
Meutevive <christianmbappe033@gmail.com>
2023-02-11 00:16:56 +0100

    ✨  bio actor page , one section remaining, but almost done

--- client/src/views/biographie/test/Data.js
+++ client/src/views/biographie/test/Data.js
@@ -1,13 +1,35 @@
 export const Contenu = {
     name:"Kit Harington",
-    Civil_status:"Christopher Catesby Harington",
+    Civil_status:["Christopher Catesby Harington"],
     Country:"Britannique",
     Birthday:"26 décembre 1986(Londres - Grande-Bretagne)",
     Age:"35",
     Biographie:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat\n" +
         "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat\n" +
-        "\n",
-
-
+        "\n"
 
 }
+
+
+export const KhPhotos =[
+    {
+        photo:"/assets/images/KH1.jpeg"
+
+    },
+    {
+        photo:"/assests/images/KH2.jpeg"
+
+    },
+    {
+        photo:"/assests/images/KH3.jpeg"
+
+    },
+    {
+        photo:"/assests/images/KH4.jpeg"
+
+    },
+]
+
+
+
+
