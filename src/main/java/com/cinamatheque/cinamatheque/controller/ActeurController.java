57c3a865bc0f1191e44f932efcb83ec912352376
delm0128 <e2palmes@gmail.com>
2023-03-15 17:42:55 +0100

    search actors by fullname

--- src/main/java/com/cinamatheque/cinamatheque/controller/ActeurController.java
+++ src/main/java/com/cinamatheque/cinamatheque/controller/ActeurController.java
@@ -3,33 +3,22 @@ package com.cinamatheque.cinamatheque.controller;
 import com.cinamatheque.cinamatheque.model.Acteur;
 import com.cinamatheque.cinamatheque.repository.ActeurRepository;
 import com.cinamatheque.cinamatheque.service.ActeurService;
-import com.cronutils.utils.StringUtils;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/acteur")
 @AllArgsConstructor
 public class ActeurController {
 
-    @Autowired
-    private ActeurService acteurService;
-    @Autowired
-    private ActeurRepository repository;
+    public ActeurService acteurService;
+    public ActeurRepository repository;
+
     // Create actor to populate actor
     // add image actor
-    // respect the field name in front application
    @PostMapping
    public Acteur CreateActeur (@RequestParam("file") MultipartFile file,
                               @RequestParam("fullname") String fullname,
@@ -47,15 +36,11 @@ public class ActeurController {
         return repository.findAll();
     }
 
-    //    get actor by id
-    @GetMapping("/search/{id}")
-    public Acteur findActeurById(@PathVariable String id){
-        return repository.findById(id).get();
-    }
     //    get actor by firstname
-    @GetMapping("/search/{fullname}")
-    public List<Acteur> findActeurByFullname(@PathVariable String fullname){
-        return repository.findByFullname(fullname);
+    @GetMapping("/search")
+    public List<Acteur> findActeurByFullname(@RequestParam String fullname){
+       System.out.print(fullname);
+        return repository.findByFullnameLike(fullname);
     }
 
     // modify existing actor inside database
@@ -63,12 +48,13 @@ public class ActeurController {
     public Acteur modifyActeur(@RequestBody Acteur acteurRequest){
         //get the existing document from DB
         //populate new value from request to existing object/entity/document
-        Acteur existingActeur = repository.findById(acteurRequest.getId()).get();
+        Acteur existingActeur = repository.findById(acteurRequest.getId()).stream().findFirst().orElse(null);
         existingActeur.setFullname(acteurRequest.getFullname());
         existingActeur.setBirthdate(acteurRequest.getBirthdate());
         existingActeur.setDescription(acteurRequest.getDescription());
         return repository.save(existingActeur);
     }
+
     //deleting actor inside database
     @DeleteMapping("/{id}")
     public String removeActeur(@PathVariable String id){
